a5bff7f36e8e9d84e031a455353b552ec835f92c
Will <wilmc17@gmail.com>
2019-11-13 11:41:05 -0800

    Adding lab 7 starter code

---
+++ lab7/include/db_credentials.php
@@ -0,0 +1,7 @@
+<?php
+	$username = "<INSERT YOUR USERNAME>";
+	$password = "<YOUR PASSWORD>";
+	$database = "db_" . $username;
+	$server = "sql04.ok.ubc.ca";
+	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
+?>
