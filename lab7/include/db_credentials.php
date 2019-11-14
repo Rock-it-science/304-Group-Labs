e9ce309b24219a06ba2cefe537b5aaf734fcc8c1
Rock-it-science <wilmc17@gmail.com>
2019-11-14 13:27:05 -0800

    Removing php stuff cus we ain't using that

--- lab7/include/db_credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$username = "<INSERT YOUR USERNAME>";
-	$password = "<YOUR PASSWORD>";
-	$database = "db_" . $username;
-	$server = "sql04.ok.ubc.ca";
-	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
-?>
